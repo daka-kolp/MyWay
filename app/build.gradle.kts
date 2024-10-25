218aae3c16bdbbfa75715d9c4743d133a176ecba
Daria Kolpakova <daka.kolp01@gmail.com>
2024-10-25 12:43:52 +0300

    dagger.hilt added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("org.jetbrains.kotlin.kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -40,4 +42,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.hilt.android)
+    kapt (libs.hilt.compiler)
 }
--- app/src/main/java/com/dkolp/myway/MyWayApplication.kt
+++ app/src/main/java/com/dkolp/myway/MyWayApplication.kt
@@ -1,5 +1,7 @@
 package com.dkolp.myway
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MyWayApplication : Application()
